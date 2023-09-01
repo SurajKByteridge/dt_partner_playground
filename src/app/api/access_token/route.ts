2a039a61d65c28e9cd89ddc8e90bbf384fd74537
SurajkByteridge <surajk@byteridge.com>
2023-09-01 15:52:21 +0530

    testing token screen 2

--- src/app/api/access_token/route.ts
+++ src/app/api/access_token/route.ts
@@ -4,31 +4,34 @@ import { format } from "url";
 
 export async function POST(request: Request, response: Request) {
   try {
-    // const data = await request.json();
+    const data = await request.json();
 
-    // var myHeaders: HeadersInit = new Headers();
-    // myHeaders.set("Content-Type", "application/x-www-form-urlencoded");
-    // myHeaders.set("Access-Control-Allow-Origin", "*");
+    var myHeaders: HeadersInit = new Headers();
+    myHeaders.set("Content-Type", "application/x-www-form-urlencoded");
+    myHeaders.set("Access-Control-Allow-Origin", "*");
 
-    // var urlencoded = new URLSearchParams();
-    // urlencoded.append("code", data.code);
-    // urlencoded.append("client_id", data.client_id);
-    // urlencoded.append("grant_type", data.grant_type);
-    // urlencoded.append("redirect_uri", data.redirect_uri);
-    // urlencoded.append("client_secret", data.client_secret);
+    var urlencoded = new URLSearchParams();
+    urlencoded.append("code", data.code);
+    urlencoded.append("client_id", data.client_id);
+    urlencoded.append("grant_type", data.grant_type);
+    urlencoded.append("redirect_uri", data.redirect_uri);
+    urlencoded.append("client_secret", data.client_secret);
 
-    // var requestOptions: RequestInit = {
-    //   method: "POST",
-    //   headers: myHeaders,
-    //   mode: "no-cors",
-    //   body: urlencoded,
-    //   redirect: "follow",
-    // };
+    var requestOptions: RequestInit = {
+      method: "POST",
+      headers: myHeaders,
+      body: urlencoded,
+      redirect: "follow",
+    };
 
-    // const fetchedData = await fetch(
-    //   "http://localhost:8080/realms/devicethread/protocol/openid-connect/token",
-    //   requestOptions
-    // );
+    const kcTokenUrl = format({
+      protocol: "http",
+      port: 8080,
+      hostname: "localhost",
+      pathname: "/realms/devicethread/protocol/openid-connect/token",
+    });
+
+    const fetchedData = await fetch(kcTokenUrl, requestOptions);
 
     // let dataRes = JSON.parse(await fetchedData.text());
 
