f384214a36e324669aec88462dfbc19981477ecf
SurajkByteridge <surajk@byteridge.com>
2023-09-01 15:46:27 +0530

    testing token screen

--- src/app/api/access_token/route.ts
+++ src/app/api/access_token/route.ts
@@ -4,35 +4,35 @@ import { format } from "url";
 
 export async function POST(request: Request, response: Request) {
   try {
-    const data = await request.json();
+    // const data = await request.json();
 
-    var myHeaders: HeadersInit = new Headers();
-    myHeaders.set("Content-Type", "application/x-www-form-urlencoded");
-    myHeaders.set("Access-Control-Allow-Origin", "*");
+    // var myHeaders: HeadersInit = new Headers();
+    // myHeaders.set("Content-Type", "application/x-www-form-urlencoded");
+    // myHeaders.set("Access-Control-Allow-Origin", "*");
 
-    var urlencoded = new URLSearchParams();
-    urlencoded.append("code", data.code);
-    urlencoded.append("client_id", data.client_id);
-    urlencoded.append("grant_type", data.grant_type);
-    urlencoded.append("redirect_uri", data.redirect_uri);
-    urlencoded.append("client_secret", data.client_secret);
+    // var urlencoded = new URLSearchParams();
+    // urlencoded.append("code", data.code);
+    // urlencoded.append("client_id", data.client_id);
+    // urlencoded.append("grant_type", data.grant_type);
+    // urlencoded.append("redirect_uri", data.redirect_uri);
+    // urlencoded.append("client_secret", data.client_secret);
 
-    var requestOptions: RequestInit = {
-      method: "POST",
-      headers: myHeaders,
-      mode: "no-cors",
-      body: urlencoded,
-      redirect: "follow",
-    };
+    // var requestOptions: RequestInit = {
+    //   method: "POST",
+    //   headers: myHeaders,
+    //   mode: "no-cors",
+    //   body: urlencoded,
+    //   redirect: "follow",
+    // };
 
-    const fetchedData = await fetch(
-      "http://localhost:8080/realms/devicethread/protocol/openid-connect/token",
-      requestOptions
-    );
+    // const fetchedData = await fetch(
+    //   "http://localhost:8080/realms/devicethread/protocol/openid-connect/token",
+    //   requestOptions
+    // );
 
-    // let dataRes = await fetchedData.text();
+    // let dataRes = JSON.parse(await fetchedData.text());
 
-    return NextResponse.json({ data: "hello" });
+    return NextResponse.json({ data: "Hello" });
   } catch (error) {
     console.log(error);
   }
