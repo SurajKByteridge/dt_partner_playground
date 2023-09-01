a4484db0aaaa5e01f6002e52ae7c6bc32748281c
SurajkByteridge <surajk@byteridge.com>
2023-09-01 16:16:11 +0530

    test kcTokenUrl

--- src/app/api/access_token/route.ts
+++ src/app/api/access_token/route.ts
@@ -36,19 +36,19 @@ export async function POST(request: Request, response: Request) {
     hostname: "localhost",
     pathname: "/realms/devicethread/protocol/openid-connect/token",
   });
-  fetch(kcTokenUrl, requestOptions)
-    .then((response) => response.text())
-    .then((result) => {
-      console.log("data", result);
-      return NextResponse.json({ data: "Hello" });
-    })
-    .catch((error) => {
-      console.log("kc access token", error);
-      return NextResponse.json({ data: "Hello" });
-    });
+  // fetch(kcTokenUrl, requestOptions)
+  //   .then((response) => response.text())
+  //   .then((result) => {
+  //     console.log("data", result);
+  //     return NextResponse.json({ data: "Hello" });
+  //   })
+  //   .catch((error) => {
+  //     console.log("kc access token", error);
+  //     return NextResponse.json({ data: "Hello" });
+  //   });
   // const fetchedData = await fetch(kcTokenUrl, requestOptions);
 
   // let dataRes = JSON.parse(await fetchedData.text());
 
-  // return NextResponse.json({ data: "Hello" });
+  return NextResponse.json({ data: "Hello" });
 }
