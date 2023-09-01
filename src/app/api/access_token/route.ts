3912d3d3c81435058d1a303f40b507c4fb433cb4
SurajkByteridge <surajk@byteridge.com>
2023-09-01 16:12:36 +0530

    removed try catch

--- src/app/api/access_token/route.ts
+++ src/app/api/access_token/route.ts
@@ -3,56 +3,52 @@ import { redirect } from "next/navigation";
 import { format } from "url";
 
 export async function POST(request: Request, response: Request) {
-  try {
-    console.log("Started ");
-    const data = await request.json();
+  console.log("Started ");
+  const data = await request.json();
 
-    var myHeaders: HeadersInit = new Headers();
-    myHeaders.set("Content-Type", "application/x-www-form-urlencoded");
-    myHeaders.set("Access-Control-Allow-Origin", "*");
+  var myHeaders: HeadersInit = new Headers();
+  myHeaders.set("Content-Type", "application/x-www-form-urlencoded");
+  myHeaders.set("Access-Control-Allow-Origin", "*");
 
-    console.log("Header ",myHeaders);
+  console.log("Header ", myHeaders);
 
-    var urlencoded = new URLSearchParams();
-    urlencoded.append("code", data.code);
-    urlencoded.append("client_id", data.client_id);
-    urlencoded.append("grant_type", data.grant_type);
-    urlencoded.append("redirect_uri", data.redirect_uri);
-    urlencoded.append("client_secret", data.client_secret);
+  var urlencoded = new URLSearchParams();
+  urlencoded.append("code", data.code);
+  urlencoded.append("client_id", data.client_id);
+  urlencoded.append("grant_type", data.grant_type);
+  urlencoded.append("redirect_uri", data.redirect_uri);
+  urlencoded.append("client_secret", data.client_secret);
 
-    console.log("urlencoded ",urlencoded);
+  console.log("urlencoded ", urlencoded);
 
-    var requestOptions: RequestInit = {
-      method: "POST",
-      headers: myHeaders,
-      body: urlencoded,
-      redirect: "follow",
-    };
+  var requestOptions: RequestInit = {
+    method: "POST",
+    headers: myHeaders,
+    body: urlencoded,
+    redirect: "follow",
+  };
 
-    console.log("requestOptions ",requestOptions);
+  console.log("requestOptions ", requestOptions);
 
-    const kcTokenUrl = format({
-      protocol: "http",
-      port: 8080,
-      hostname: "localhost",
-      pathname: "/realms/devicethread/protocol/openid-connect/token",
+  const kcTokenUrl = format({
+    protocol: "http",
+    port: 8080,
+    hostname: "localhost",
+    pathname: "/realms/devicethread/protocol/openid-connect/token",
+  });
+  fetch(kcTokenUrl, requestOptions)
+    .then((response) => response.text())
+    .then((result) => {
+      console.log("data", result);
+      return NextResponse.json({ data: "Hello" });
+    })
+    .catch((error) => {
+      console.log("kc access token", error);
+      return NextResponse.json({ data: "Hello" });
     });
-    fetch(kcTokenUrl, requestOptions)
-      .then((response) => response.text())
-      .then((result) => {
-        console.log("data", result);
-        return NextResponse.json({ data: "Hello" });
-      })
-      .catch((error) => {
-        console.log("kc access token", error);
-        return NextResponse.json({ data: "Hello" });
-      });
-    // const fetchedData = await fetch(kcTokenUrl, requestOptions);
+  // const fetchedData = await fetch(kcTokenUrl, requestOptions);
 
-    // let dataRes = JSON.parse(await fetchedData.text());
+  // let dataRes = JSON.parse(await fetchedData.text());
 
-    // return NextResponse.json({ data: "Hello" });
-  } catch (error) {
-    console.log(error);
-  }
+  // return NextResponse.json({ data: "Hello" });
 }
